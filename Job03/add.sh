9ef098c1fc4b5f13e100ba480780726263b3b699
root <root@PcProWIlly>
2023-10-05 11:40:29 +0200

    devoirs

--- Job03/add.sh
+++
@@ -1,1 +0,0 @@
-echo "La somme de $1 et $2 est égale à $(($1 + $2))"
--- Job04/argument.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat "$2" > "$1" && echo "Copie terminée avec succès dans $1." || echo "Erreur lors de la copie dans $1."
-
